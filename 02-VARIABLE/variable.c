6379d2ef71b0ee294eb0f046d9975958113d6b34
Maeva De Campos <kulwilcoox@gmail.com>
2023-01-01 14:37:23 +0100

    How to request something from the user? -> scanf

--- 02-VARIABLE/variable.c
+++ 02-VARIABLE/variable.c
@@ -11,13 +11,25 @@
   printf("Ah desole, il ne vous reste plus que %d vies maintenant !\n\n", nb_lifes);
 
   return 0;
-} */
+}
 
 int main(int argc, char *argv[])
 {
   int nombreDeVies = 5, niveau = 1;
 
   printf("Vous avez %d vies et vous etes au niveau n° %d\n", nombreDeVies, niveau);
+
+  return 0;
+} */
+
+
+int main(int argc, char *argv[])
+{
+  int age = 0; // On initialise la variable à 0
+
+  printf("Quel age avez-vous ? ");
+  scanf("%d", &age); // On demande d'entrer l'âge avec scanf
+  printf("Ah ! Vous avez donc %d ans !\n\n", age);
 
   return 0;
 }
