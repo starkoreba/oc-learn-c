0862f5d5f0d517583447cf4b8cacb4bdb033bb38
Maeva De Campos <kulwilcoox@gmail.com>
2023-01-01 14:35:47 +0100

    Implementing level

--- 02-VARIABLE/variable.c
+++ 02-VARIABLE/variable.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) // Équivalent de int main()
+/* int main(int argc, char *argv[]) // Équivalent de int main()
 {
   int nb_lifes = 5;
 
@@ -9,6 +9,15 @@ int main(int argc, char *argv[]) // Équivalent de int main()
   printf("**** B A M ****\n"); // Là il se prend un grand coup sur la tête
   nb_lifes = 4; // Il vient de perdre une vie !
   printf("Ah desole, il ne vous reste plus que %d vies maintenant !\n\n", nb_lifes);
+
+  return 0;
+} */
+
+int main(int argc, char *argv[])
+{
+  int nombreDeVies = 5, niveau = 1;
+
+  printf("Vous avez %d vies et vous etes au niveau n° %d\n", nombreDeVies, niveau);
 
   return 0;
 }
