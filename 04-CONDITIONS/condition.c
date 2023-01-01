1e73bf43d9bdcae87c9a378d87f739f0ab63f6d7
Maeva De Campos <kulwilcoox@gmail.com>
2023-01-01 15:48:39 +0100

    done

--- 04-CONDITIONS/condition.c
+++ 04-CONDITIONS/condition.c
@@ -9,14 +9,18 @@ int main(int argc, char *argv[])
   int nb_lifes = 5, level = 1, difficulty = 0, damages = 1, result = 0;
 
   printf("You have %d lifes and you are level %d\n", nb_lifes, level);
-  printf("Choose a difficulty 1-3 :\n");
+  printf("Choose a difficulty 1-5 :\n");
   scanf("%d", &difficulty);
 
   damages *= difficulty;
   result = nb_lifes - damages;
 
   printf("You choose difficulty %d take %d damages \n", difficulty, damages);
+
+  if(result > 0)
   printf("You survived with %d lifes\n", result);
+  else
+  printf("You loose\n");
 
   return 0;
 }
