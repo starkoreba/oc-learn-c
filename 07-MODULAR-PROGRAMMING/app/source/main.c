197bf811f693439b098c39048d3e38b2932e02a6
Maeva De Campos <kulwilcoox@gmail.com>
2023-01-03 16:10:20 +0100

    Initial commit modular programming
    Testing  modular files structure based on:
    https://openclassrooms.com/fr/courses/19980-apprenez-a-programmer-en-c/7528516-initiez-vous-a-la-programmation-modulaire

---
+++ 07-MODULAR-PROGRAMMING/app/source/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// TODO : Enhance your code with functions
+
+int real_damage(int damages, int diff)
+{
+  return damages *= diff;
+}
+int main(int argc, char *argv[])
+{
+  int nb_lifes = 5, level = 1, difficulty = 0, damages = 1, result = 0;
+
+  printf("You have %d lifes and you are level %d\n", nb_lifes, level);
+  do
+  {
+    printf("Choose a difficulty 1-5 :\n");
+    scanf("%d", &difficulty);
+  } while (difficulty > 5 || difficulty == 0);
+
+
+  damages = real_damage(damages, difficulty);
+  result = nb_lifes - damages;
+
+  printf("You choose difficulty %d take %d damages \n", difficulty, damages);
+
+  if(result > 0)
+  printf("You survived with %d lifes\n", result);
+  else
+  printf("You loose\n");
+
+  return 0;
+}
