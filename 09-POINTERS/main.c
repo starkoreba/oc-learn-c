cd41d8402e3ebdcaaddaae6c9d520fe214a6d999
Maeva De Campos <kulwilcoox@gmail.com>
2023-01-04 17:12:46 +0100

    Display hours and minutes
    Use void function with pointers to splitMinutes in hours and minutes

--- 09-POINTERS/main.c
+++ 09-POINTERS/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+void splitMinutes(int *ptHours, int *ptMinutes);
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
-  int age = 21;
-  int *ptage = &age;
+  int hours = 1, minutes = 90;
 
-  printf("%d", *ptage);
+  splitMinutes(&hours, &minutes);
+
+  printf("%d heures et %d minutes", hours, minutes);
 
   return 0;
+}
+
+void splitMinutes(int *ptHours, int *ptMinutes)
+{
+  *ptHours = *ptMinutes / 60;
+  *ptMinutes = *ptMinutes % 60;
 }
